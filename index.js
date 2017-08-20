a0daf4d85e602840b6e674f8bd6357d4ea2d4f5f
irk <irk@svetlana.local>
2017-08-20 18:03:13 -0500

    buildContext.js: finally gets writeJsonFromContext working

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var fs      = require('fs');
+var path = require('path');
 
 if (!fs.existsSync('dist')){
     fs.mkdirSync('dist');
 }
 
-require('./lib/get_gtfs.js');
+//require('./lib/getGtfs.js');
